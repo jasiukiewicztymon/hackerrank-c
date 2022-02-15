f353114ec8840316a99d857f24200c5907398cf6
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-15 13:50:49 +0100

    Update Conditional Statements in C.c

--- Conditional Statements in C.c
+++ Conditional Statements in C.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-const char *strings[] = {"one","two","three","four","five","six","seven","eight","nine"};
+const char *names[] = {"one","two","three","four","five","six","seven","eight","nine"};
 int main()
 {
     int a = 0;
@@ -9,7 +9,7 @@ int main()
         return 1;
     
     if (a >= 1 && a <= 9)
-        printf("%s",strings[a-1]);
+        printf("%s",names[a-1]);
     else
         printf("Greater than 9");
     
